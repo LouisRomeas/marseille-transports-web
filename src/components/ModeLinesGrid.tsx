cc9bda9debf080c380ad59acef4f422cfafe0622
Louis Roméas <louis.romeas@gmail.com>
2022-11-06 16:46:00 +0100

    spinner anim

--- src/components/ModeLinesGrid.tsx
+++ src/components/ModeLinesGrid.tsx
@@ -2,6 +2,8 @@ import { Component } from 'react'
 import { Line, TransportMode } from '../types'
 import LineSquare from './LineSquare'
 import '../style/ModeLinesGrid.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 
 type ModeLinesGridProps = {
   mode: TransportMode,
@@ -17,11 +19,12 @@ export default class ModeLinesGrid extends Component<ModeLinesGridProps, ModeLin
     return (
       <div className={`mode-lines-grid ${this.props.mode}`}>
         {
-          this.props.lines.map((line, index) => {
-            return (
-              <LineSquare key={index} line={line} />
+          this.props.lines.map((line, index) => (
+            this.props.lines.length ?
+              <LineSquare key={index} line={line} /> :
+              <FontAwesomeIcon icon={faSpinner} fontSize='2em' className='spinner' />
             )
-          })
+          )
         }
       </div>
     )
