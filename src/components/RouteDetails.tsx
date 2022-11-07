7a359c709a6f5e20812df0cc2fda55503c96fa4c
Louis Roméas <louis.romeas@gmail.com>
2022-11-07 20:37:38 +0100

    add react-leaflet

--- src/components/RouteDetails.tsx
+++ src/components/RouteDetails.tsx
@@ -4,6 +4,8 @@ import { Component, CSSProperties } from 'react'
 import { Line, Route, Stop } from '../types'
 import '../style/RouteDetails.scss';
 import StopInfo from './StopInfo';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
 
 type RouteDetailsProps = {
   route: Route,
@@ -41,18 +43,35 @@ export default class RouteDetails extends Component<RouteDetailsProps, RouteDeta
   render() {
     return (
       <div className='route-details' style={{ '--line-color': this.props.color } as CSSProperties}>
-        <div className="title">Direction : <span className="route-direction">{this.props.route.DirectionStations}</span></div>
-        <ul className="stops">
-          {
-            this.state.stops.length ?
-              this.state.stops.map((stop: Stop, index) => (
-                <StopInfo key={index} stop={stop} line={this.props.line} color={this.props.color} />
-              )) :
-              (
-                <FontAwesomeIcon icon={faSpinner} fontSize='2em' className='spinner' />
-              )
-          }
-        </ul>
+        <div className="content">
+          <div className="stops-wrapper">
+            <div className="title">Direction : <span className="route-direction">{this.props.route.DirectionStations}</span></div>
+            <ul className="stops">
+              {
+                this.state.stops.length ?
+                  this.state.stops.map((stop: Stop, index) => (
+                    <StopInfo key={index} stop={stop} line={this.props.line} color={this.props.color} />
+                  )) :
+                  (
+                    <FontAwesomeIcon icon={faSpinner} fontSize='2em' className='spinner' />
+                  )
+              }
+            </ul>
+          </div>
+          <div className="map">
+            <MapContainer className='route-map' center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+              <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              />
+              <Marker position={[51.505, -0.09]}>
+                <Popup>
+                  A pretty CSS3 popup. <br /> Easily customizable.
+                </Popup>
+              </Marker>
+            </MapContainer>
+          </div>
+        </div>
       </div>
     )
   }
